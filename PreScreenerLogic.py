0c4d1a8db9af87a007962057bf9790bf8a929a24
Hady Salama <salama.hady@hotmail.com>
2020-04-09 05:42:19 +0000

    Hady's Second Commit

--- PreScreenerLogic.py
+++ PreScreenerLogic.py
@@ -1,8 +1,16 @@
 """
 Created on Sunday 4/9/20 ‏‎1:15:00 AM
 @authors: Hady S. Salama and Jack Lynch
+
 Personal Project
 """
 
-# this is jacks super complex code
+#This is jacks super complex code
 cough = False
+
+#This is Hady's next line of code
+temperature = 105
+temperature = False
+
+if(temperature > 100.4):
+    temperature = True
