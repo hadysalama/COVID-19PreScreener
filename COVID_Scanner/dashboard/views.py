6011933bf472a1e0cbadf5e2f3764da4b5d68bda
Hady Salama <salama.hady@hotmail.com>
2020-04-12 07:10:48 +0000

    Views Beta 1
    Signed-off-by: Hady Salama <salama.hady@hotmail.com>

--- COVID_Scanner/dashboard/views.py
+++ COVID_Scanner/dashboard/views.py
@@ -6,39 +6,105 @@ Personal Project
 
 from django.shortcuts import render
 
+
 def home(request):
     risk = ""
+    actions = ""
+    get_test = False
 
     if request.method == 'POST':
         prescreen_checks = request.POST.getlist('covid-check')
-        print(prescreen_checks)
 
         # Important symptoms
-        cough = "cough" in prescreen_checks # Dry Cough
-        print(cough)
+        cough = "cough" in prescreen_checks  # Dry Cough
+        # Implies the user has Coronavirus in some cases
+        shortness_of_breath = "sob" in prescreen_checks
+        # Greater than 100.4 (This can be subjective)
+        fever = "fever" in prescreen_checks
 
-        shortness_of_breath = "sob" in prescreen_checks # Implies the patient has Coronavirus in some cases
-        fever = "fever" in prescreen_checks # Greater than 100.4 (This can be subjective)
+        # Checks if the user has a Cleveland Clinic Identified High Risk Condition.
+        cchighrisk = request.POST.get("cchighrisk")
+        if cchighrisk is not "None":
+            cchighrisk = True
+        else:
+            cchighrisk = False
 
         # Higher Risk Options
-        travel = "travel" in prescreen_checks # User has traveled to an area heavily impacted by COVID-19.
-        lab_covid_case = "contact-positive-case" in prescreen_checks # User has came into contact with a laboratory-confirmed case of COVID-19.
-        family_member_covid = "family-contact-positive-case" in prescreen_checks # User has came into contact with a family memeber or close person that has confirmed COVID-19 outside a healthcare facility.
-        household_precautions = "follow-precautions" in prescreen_checks # Recommended precautions for home care and isolation followed consistently.
-        family_member_covid_6feet = "within-6-feet-positive-case" in prescreen_checks # User has been in a confined indoor enviornemnt with a confirmed case greater than 6 feet apart.
-        family_member_covid_confined_env = "more-6-feet-positive-case" in prescreen_checks #In a confined enviornment less than 6 feet apart.
+        # User has traveled to an area heavily impacted by COVID-19.
+        travel = "travel" in prescreen_checks
+        # User has came into contact with a laboratory-confirmed case of COVID-19.
+        lab_covid_case = "contact-positive-case" in prescreen_checks
+        # User has came into contact with a family memeber or close person that has confirmed COVID-19 outside a healthcare facility.
+        family_member_covid = "family-contact-positive-case" in prescreen_checks
+        # Recommended precautions for home care and isolation followed consistently.
+        household_precautions = "follow-precautions" in prescreen_checks
+        # User has been in a confined indoor enviornemnt with a confirmed case greater than 6 feet apart.
+        family_member_covid_6feet = "within-6-feet-positive-case" in prescreen_checks
+        # User is in a confined enviornment less than 6 feet apart.
+        family_member_covid_confined_env = "more-6-feet-positive-case" in prescreen_checks
 
-        symptomatic = False #Symptomatic due to Cleveland Clinic Screening
-        
+        # Symptomatic due to Cleveland Clinic Screening
+        symptomatic = cough or shortness_of_breath or fever
 
-        if((not travel) and (not lab_covid_case) and (not symptomatic)):
-            risk = "None"
-        elif((not travel) and (not lab_covid_case) and symptomatic):
-            risk = "None; routine medical care"
-        elif((not travel) and (lab_covid_case)  and (not family_member_covid) and (not family_member_covid_6feet) and symptomatic):
-            risk = "None; routine medical care"
+        # Nested ifs correspond to the CDC Tree Diagram
+
+        get_test = (cough and fever and cchighrisk) or (cough and shortness_of_breath and cchighrisk) or (
+            fever and cchighrisk) or (shortness_of_breath and cchighrisk)
+
+        if travel:
+            if lab_covid_case:
+                risk = "High Risk"
+                if symptomatic:
+                    actions = "Immediate isolation; medical evaluation guided by PUI Definition; pre-notify healthcare services; controlled travel"
+                else:
+                    actions = "Remain under quarantine authority; no public activities; daily active monitoring; controlled travel"
+            else:
+                risk = "Medium Risk"
+                if symptomatic:
+                    actions = "Immediate isolation; medical evaluation guided by PUI definition; pre-notify healthcare services; control travel"
+                else:
+                    actions = "Stay home; active monitoring or self muttering with public health supervision; recommended to not travel"
         else:
-            risk = "You are at high risk for COVID-19."
+            if lab_covid_case:
+                if family_member_covid:
+                    if household_precautions:
+                        risk = "Medium Risk"
+                        if symptomatic:
+                            actions = "Immediate isolation; medical evaluation guided by PUI definition; pre-notify healthcare services; control travel"
+                        else:
+                            actions = "Stay home; active monitoring or self muttering with public health supervision; recommended to not travel"
+                    else:
+                        risk = "High Risk"
+                        if symptomatic:
+                            actions = "Immediate isolation; medical evaluation guided by PUI Definition; pre-notify healthcare services; controlled travel"
+                        else:
+                            actions = "Remain under quarantine authority; no public activities; daily active monitoring; controlled travel"
+                else:
+                    if family_member_covid_6feet:
+                        risk = "Medium Risk"
+                        if symptomatic:
+                            actions = "Immediate isolation; medical evaluation guided by PUI definition; pre-notify healthcare services; control travel"
+                        else:
+                            actions = "Stay home; active monitoring or self muttering with public health supervision; recommended to not travel"
+                    else:
+                        if family_member_covid_confined_env:
+                            risk = "Low Risk"
+                            if symptomatic:
+                                actions = "Stay home from work or school, avoid contact with others, don't travel. Seek health advice"
+                            else:
+                                actions = "Self observation"
+                        else:
+                            risk = "No Identifiable Risk"
+                            if symptomatic:
+                                actions = "None; routine medical care"
+                            else:
+                                actions = "None"
+            else:
+                risk = "No Identifiable Risk"
+                if symptomatic:
+                    actions = "None; routine medical care"
+                else:
+                    actions = "None"
 
-    context = {'risk': risk}
+    context = {'risk': risk, 'actions': actions, 'get-test': get_test}
     return render(request, 'dashboard/home.html', context)
