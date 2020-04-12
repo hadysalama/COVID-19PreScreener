c51efd89c912b3e1c05cdaed0c4b6701bbb21b29
Hady Salama <salama.hady@hotmail.com>
2020-04-12 06:05:36 +0000

    First Working Django App
    Signed-off-by: Hady Salama <salama.hady@hotmail.com>

--- COVID_Scanner/dashboard/views.py
+++ COVID_Scanner/dashboard/views.py
@@ -7,35 +7,38 @@ Personal Project
 from django.shortcuts import render
 
 def home(request):
-
-    # Important symptoms
-    cough = False # Dry Cough
-    shortness_of_breath = False # Implies the patient has Coronavirus in some cases
-    fever = False # Greater than 100.4 (This can be subjective)
-
-    # Higher Risk Options
-    travel = False # User has traveled to an area heavily impacted by COVID-19.
-    lab_covid_case = False # User has came into contact with a laboratory-confirmed case of COVID-19.
-    family_member_covid = False # User has came into contact with a family memeber or close person that has confirmed COVID-19 outside a healthcare facility.
-    household_precautions = False # Recommended precautions for home care and isolation followed consistently.
-    family_member_covid_6feet = False # User has been in a confined indoor enviornemnt with a confirmed case greater than 6 feet apart.
-    family_member_covid_confined_env = False #In a confined enviornment less than 6 feet apart.
-
-    symptomatic = False #Symptomatic due to Cleveland Clinic Screening
     risk = ""
-
-    if((not travel) and (not lab_covid_case) and (not symptomatic)):
-        risk = "None"
-    elif((not travel) and (not lab_covid_case) and symptomatic):
-        risk = "None; routine medical care"
-    elif((not travel) and (lab_covid_case)  and (not family_member_covid) and (not family_member_covid_6feet) and symptomatic):
-        risk = "None; routine medical care"
-
-
 
     if request.method == 'POST':
         prescreen_checks = request.POST.getlist('covid-check')
         print(prescreen_checks)
 
-    context = {}
+        # Important symptoms
+        cough = "cough" in prescreen_checks # Dry Cough
+        print(cough)
+
+        shortness_of_breath = "sob" in prescreen_checks # Implies the patient has Coronavirus in some cases
+        fever = "fever" in prescreen_checks # Greater than 100.4 (This can be subjective)
+
+        # Higher Risk Options
+        travel = "travel" in prescreen_checks # User has traveled to an area heavily impacted by COVID-19.
+        lab_covid_case = "contact-positive-case" in prescreen_checks # User has came into contact with a laboratory-confirmed case of COVID-19.
+        family_member_covid = "family-contact-positive-case" in prescreen_checks # User has came into contact with a family memeber or close person that has confirmed COVID-19 outside a healthcare facility.
+        household_precautions = "follow-precautions" in prescreen_checks # Recommended precautions for home care and isolation followed consistently.
+        family_member_covid_6feet = "within-6-feet-positive-case" in prescreen_checks # User has been in a confined indoor enviornemnt with a confirmed case greater than 6 feet apart.
+        family_member_covid_confined_env = "more-6-feet-positive-case" in prescreen_checks #In a confined enviornment less than 6 feet apart.
+
+        symptomatic = False #Symptomatic due to Cleveland Clinic Screening
+        
+
+        if((not travel) and (not lab_covid_case) and (not symptomatic)):
+            risk = "None"
+        elif((not travel) and (not lab_covid_case) and symptomatic):
+            risk = "None; routine medical care"
+        elif((not travel) and (lab_covid_case)  and (not family_member_covid) and (not family_member_covid_6feet) and symptomatic):
+            risk = "None; routine medical care"
+        else:
+            risk = "You are at high risk for COVID-19."
+
+    context = {'risk': risk}
     return render(request, 'dashboard/home.html', context)
